70347f6e9fe2ed125c553fc1e8beda515300ed9b
ChiefLopes <chieflopesekede@gmail.com>
2024-04-20 09:36:14 +0100

    navabr dropdown

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
-    <div className="flex items-center py-2 mx-10 lg:mx-6">
+    <div className="h-[4rem] flex items-center py-3 mx-10 lg:mx-6">
       <Image
         src={Karepal}
         alt="logo"
@@ -19,7 +19,8 @@ const Navbar = () => {
         priority
         className="lg:ml-9 cursor-pointer"
       />
-      <div className="hidden lg:flex space-x-8 pl-[14rem] lg:pt-2 ">
+
+      <div className="hidden lg:flex space-x-8 pl-[14rem] lg:pt-4 ">
         {NAV_LINKS.map((link) => (
           <a key={link.href} href={link.href} className="pt-2">
             <p className="flex">{link.label}</p>
@@ -36,19 +37,39 @@ const Navbar = () => {
       </div>
 
       <div
-        className="lg:hidden ml-5 font-bold text-xl cursor-pointer "
+        className={`lg:hidden ml-5 font-bold text-xl cursor-pointer ${
+          toggleMenu
+            ? "transition-opacity ease-out duration-300 opacity-100"
+            : "transition-opacity ease-in duration-300 opacity-100"
+        }`}
         onClick={() => setToggleMenu(!toggleMenu)}
       >
         {toggleMenu ? 1 : 2}
       </div>
 
-      <div>
+      <div className="relative ">
         {toggleMenu && (
-          <ul className="bg-red-500 w-32 h-32 flex flex-col pt-14 lg:hidden">
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-          </ul>
+          <div
+            className={`bg-white text-[#001727] w-[13rem] h-[17rem] mt-[8rem] px-4 z-40 flex flex-col lg:hidden relative right-12 top-20 rounded-s-lg ${
+              toggleMenu ? "" : ""
+            }`}
+          >
+            <div
+              className="text-end me-2 text-2xl cursor-pointer"
+              onClick={() => setToggleMenu(false)}
+            >
+              X
+            </div>
+            <div className="">
+              {NAV_LINKS.map((nav, index) => (
+                <Link href={nav.href} key={index} className="space-y-4">
+                  <p className="font-bold py-1 hover:text-red-400">
+                    {nav.label}
+                  </p>
+                </Link>
+              ))}
+            </div>
+          </div>
         )}
       </div>
     </div>
