bc9fe6374fcf0086909f3b6c85e400a42698f342
ChiefLopes <chieflopesekede@gmail.com>
2024-04-23 03:13:35 +0100

    updated the home page

--- app/pages/home.tsx
+++ app/pages/home.tsx
@@ -7,15 +7,15 @@ const Hero = () => {
   return (
     <div className="bg-[#001727] lg:h-[38.6rem]">
       {/* TEXT AREA*/}
-      <div className="flex flex-col md:flex-row lg:flex-row lg:justify-between pt-[5.6rem] lg:pt-[12rem] mx-10 lg:mx-14 lg:space-x-6">
+      <div className="flex flex-col md:flex-row lg:flex-row lg:justify-between pt-[5.6rem] lg:pt-[12rem] mx-4 lg:mx-14 lg:space-x-6">
         <div className="text-white space-y-6 lg:space-y-12 pt-2">
           <h1 className="text-2xl lg:text-4xl font-semibold playfair-font">
             Transforming Care giving <br /> through data-driven AI tools
           </h1>
           <div>
-            <p className="font-semibold">
-              Karepal&apos;s AI-powered care giving services provides <br />{" "}
-              numerous services to both care givers <br /> and care recipients
+            <p className="font-semibold lg:max-w-[24rem]">
+              Karepal&apos;s AI-powered care-giving services provides numerous
+              services to both care-givers and care recipients
             </p>
           </div>
           <div className="flex space-x-10">
