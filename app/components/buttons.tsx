598d95cf65db38e189357d64a0b9142de5b3fdb0
ChiefLopes <chieflopesekede@gmail.com>
2024-04-23 04:23:43 +0100

    updated buttons

--- app/components/buttons.tsx
+++ app/components/buttons.tsx
@@ -1,16 +1,41 @@
-import React from 'react'
+import { useState } from "react";
 
 const Buttons = () => {
+  type btn = boolean | string;
+
+  const [btn, setBtn] = useState<btn>(true);
+//   const [color, setColor] = useState<btn>("blue");
+
+//   const handleChange = () => {
+//     setBtn(!btn);
+//     // setColor("");
+//   };
+
   return (
     <div className="flex space-x-5 pb-1">
-      <button className="border w-[6rem] h-9 rounded-lg border-[#2937B6] hover:bg-[#2937B6] hover:text-white transition duration-200">
+      {/* SIGN IN BUTTON */}
+      <button
+        className={`border w-[6rem] h-9 rounded-lg border-[#2937B6]  hover:bg-[#2937B6] text-black transition duration-200  ${
+          btn ? "bg-[#2937B6] transition ease-in-out duration-300" : ""
+        }`}
+        onMouseEnter={() => setBtn(true)}
+        onMouseLeave={() => setBtn(false)}
+      >
         Sign In
       </button>
-      <button className="border w-[6rem] h-9 rounded-lg bg-[#2937B6] text-white hover:bg-transparent hover:text-black hover:border-[#2937b6] transition duration-200">
+
+      {/* SIGN UP BUTTON */}
+      <button
+        className={`border w-[6rem] h-9 rounded-lg border-[#2937B6]  bg-[#2937B6] hover:text-white transition duration-300 ease-in-out ${
+          btn ? "bg-white text-black transition duration-300 ease-in-out" : ""
+        }`}
+        // onMouseEnter={() => setBtn(false)}
+        // onMouseLeave={() => setBtn(true)}
+      >
         Sign Up
       </button>
     </div>
   );
-}
+};
 
-export default Buttons
+export default Buttons;
