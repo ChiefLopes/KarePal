ddc532219449b0b5ff1762bd562f2e36b52e26d7
ChiefLopes <chieflopesekede@gmail.com>
2024-05-06 17:12:09 +0100

    added lucide-react and also a loader function at the Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import LandingPage from "./landingPage";
 import Image from "next/image";
 import Navbar from "./components/navbar";
@@ -8,11 +10,29 @@ import Team from "./pages/team";
 import Newsletter from "./pages/newsletter";
 import Testimonials from "./pages/testmonials";
 import Footer from "./pages/footer";
+import { Loader } from "lucide-react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading delay with setTimeout
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 200); // Change the delay time as needed (in milliseconds)
+
+    // Cleanup function to clear the timer if the component unmounts before loading completes
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div>
-      <LandingPage />
+    <div className={isLoading ? "bg-[#001727] w-screen h-screen flex items-center justify-center" : ""}>
+      {isLoading ? (
+        <Loader className="w-20 h-20 text-slate-400 flex mx-auto animate-spin" />
+      ) : (
+        <LandingPage />
+      )}
     </div>
   );
 }
