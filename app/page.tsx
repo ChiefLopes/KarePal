24198fe55903312e0800e7cb8b5499616d54a995
ChiefLopes <chieflopesekede@gmail.com>
2024-05-06 17:24:53 +0100

    added lucide-react and also a loader function at the Home component

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,9 @@ import Team from "./pages/team";
 import Newsletter from "./pages/newsletter";
 import Testimonials from "./pages/testmonials";
 import Footer from "./pages/footer";
-import { Loader } from "lucide-react";
+import { LoaderPinwheel } from "lucide-react";
 import { useState, useEffect } from "react";
+import { LuLoader } from "react-icons/lu";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
@@ -20,16 +21,25 @@ export default function Home() {
     // Simulate loading delay with setTimeout
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 200); // Change the delay time as needed (in milliseconds)
+    }, 100); // Change the delay time as needed (in milliseconds)
 
     // Cleanup function to clear the timer if the component unmounts before loading completes
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <div className={isLoading ? "bg-[#001727] w-screen h-screen flex items-center justify-center" : ""}>
+    <div
+      className={
+        isLoading
+          ? "bg-[#001727] w-screen h-screen flex items-center justify-center"
+          : ""
+      }
+    >
       {isLoading ? (
-        <Loader className="w-20 h-20 text-slate-400 flex mx-auto animate-spin" />
+        <div>
+          <LoaderPinwheel className="w-20 h-20 text-slate-400 flex mx-auto animate-spin" />
+          <span className="text-white font-bold text-xl"> Welcome to Karepal... </span>
+        </div>
       ) : (
         <LandingPage />
       )}
